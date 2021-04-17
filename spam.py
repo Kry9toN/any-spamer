38cee57c26b8d37318fbe016d323cec601cfec88
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-04-17 11:07:40 +0700

    initial git and upload

---
+++ spam.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+word = open('bigtext.txt', 'r')
+def spam():
+    for text in word:
+        time.sleep(.1)
+        pyautogui.typewrite(text)
+        pyautogui.press('enter')
+
+if __name__ == "__main__":
+    print('Spam starting in 10 sec')
+    print('Open WA on browser now')
+    time.sleep(10)
+    print('Spamming....')
+    spam()
